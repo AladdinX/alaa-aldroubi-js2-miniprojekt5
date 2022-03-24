cc9d86457c77ca1b0909bc9b05a25dadf4a7ff60
AladdinX <Alaa.aldroubi@gmail.com>
2022-03-24 20:09:30 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { Comment } from "./mod/Comment";
 import { db } from "./mod/firebase"
 
 const dbRef = ref(db, '/Comments');
-const userNameValue = (document.querySelector('#user-name') as HTMLInputElement).value;
+const userNameValue = document.querySelector('#user-name') as HTMLInputElement;
 let comments: Comment[] = [];
 
 document.querySelector('#start-button').addEventListener('click', () => {
   (document.querySelector('#start-div') as HTMLDivElement).style.display = 'none';
-  (document.querySelector('#welcome-massage') as HTMLHeadingElement).innerText = `Hello ${userNameValue} if u want to delete your old comments click on it !!!`;
+  (document.querySelector('#welcome-massage') as HTMLHeadingElement).innerText = `Hello ${userNameValue.value} if u want to delete your old comments click on it !!!`;
   onValue(dbRef, snapshot => {
     (document.querySelector('#comment-container') as HTMLDivElement).style.display = 'inline-block';
     const commentsData = snapshot.val();
@@ -24,7 +24,7 @@ document.querySelector('#start-button').addEventListener('click', () => {
       ));
     }
     for (const comment of comments) {
-      if (comment.name == userNameValue) {
+      if (comment.name == userNameValue.value) {
         (document.querySelector(`#${comment.id}`) as HTMLHeadingElement).style.color = 'white';
         document.querySelector(`#${comment.id}`).addEventListener('click', () => {
           const deleteRef = ref(db, '/Comments/' + comment.id);
@@ -43,7 +43,7 @@ document.querySelector('#add').addEventListener('click', e => {
   }
   const commentToAdd = {
     comment: (document.querySelector("#new-comment") as HTMLInputElement).value,
-    name: userNameValue
+    name: userNameValue.value
   }
   const newKey: string = push(dbRef).key;
   const newComment = {};
